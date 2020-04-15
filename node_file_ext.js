9603bd509034bc69f825f0dd0ca5b5d274abbe6b
Bagus iMac <bagus.stimata@gmail.com>
2020-04-15 17:21:28 +0700

    oke
    oke

--- node_file_ext.js
+++ node_file_ext.js
@@ -1,41 +1,43 @@
-/*
-Latihan
-1. Cek File
-2. Open File
-3. Read File
-4. Write File
-*/
+function readWriteFile(){
+    /*
+    Latihan
+    1. Cek File
+    2. Open File
+    3. Read File
+    4. Write File
+    */
 
+    var fs = require("fs");
+    var fileName_ = 'input.txt';
+    fs.stat(fileName_, function (err, stats) {
+    if (err) {
+        return console.error(err);
+    }
 
-var fs = require("fs");
-var fileName_ = 'input.txt';
-fs.stat(fileName_, function (err, stats) {
-   if (err) {
-      return console.error(err);
-   }
-
-   //1. Cek File is Exist
-   if (stats.isFile) {
-       //2. Open File
-        fs.open(fileName_, 'r+', function(err, fd) {
-                if (err) {
-                    return console.error(err);
-                }
-
-                fs.appendFile(fileName_, '>> ini yang ditambahkan #\n', function (err) {
-                  if (err) throw err;
-                  console.log('The "data to append" was appended to file!');
-                
-                });
-  
-                // Close the opened file.
-                fs.close(fd, function(err) {
+    //1. Cek File is Exist
+    if (stats.isFile) {
+        //2. Open File
+            fs.open(fileName_, 'r+', function(err, fd) {
                     if (err) {
-                    console.log(err);
-                    } 
-                    console.log("File closed successfully.");
-                });                
-        });   
-   }
-   
-});
+                        return console.error(err);
+                    }
+
+                    fs.appendFile(fileName_, '>> ini yang ditambahkan #\n', function (err) {
+                    if (err) throw err;
+                        console.log('The "data to append" was appended to file!');                    
+                    });
+    
+                    // Close the opened file.
+                    fs.close(fd, function(err) {
+                        if (err) {
+                        console.log(err);
+                        } 
+                        console.log("File closed successfully.");
+                    });                
+            });   
+    }
+    
+    });
+}
+
+readWriteFile();
